28cb0929c896357fe47ab76517e82f362c795a21
QuantVI <berkeleyvi@gmail.com>
2019-01-16 00:54:31 +0100

    major changes from book code

--- sql_database_fill_n_retrieval/price_retrieval.py
+++ sql_database_fill_n_retrieval/price_retrieval.py
@@ -9,6 +9,9 @@ import warnings
 import mysql.connector as mdb
 import requests
 
+import pandas_datareader.data as web
+import time
+
 # Connect to the MySQL instance
 db_host = 'localhost'
 db_user = 'sec_user'
@@ -30,17 +33,10 @@ def obtain_list_of_db_tickers():
     
 #z = obtain_list_of_db_tickers()
 
-# sample url to download Microsoft dailt historical data
-# from 2018-01-13 to 2019-01-13
-    # https://query1.finance.yahoo.com/v7/finance/download/MSFT?
-    # period1=1544723038&period2=1547401438&interval=1d
-    # &events=history&crumb=IMF/v130KPT
-"https://query1.finance.yahoo.com/v7/finance/download/MSFT?period1=1544723038&period2=1547401438&interval=1d&events=history&crumb=IMF/v130KPT"
-# Changes in url consturction are necessary
-
 def get_daily_historic_data_yahoo( \
-    ticker, start_date=(2000,1,1),
-    end_date=datetime.date.today().timetuple()[0:3]):
+    ticker,
+    start_date=datetime.datetime(2000,1,1),
+    end_date=datetime.datetime.today() ):
     # print(start_date,end_date)
     """Obtains data from Yahoo Finanace returns, and a list of tuples.
 
@@ -48,14 +44,33 @@ def get_daily_historic_data_yahoo( \
     start_date: Start date in (YYYY, M, D) format
     end_date: End date in (YYYY, M, D) format"""
 
-    # Construct the Yahoo URL with the correct integer query parameters
-    # for start and end dates. Note that some parameters are zero-based.
+    # use pandas_datareader.data with the correct parameters
+    # for start and end dates.
 
-    ticker_tup = (
-        ticker, start_date[1]-1, start_date[2], start_date[0],
-        end_date[1]-1,end_date[2],end_date[0])
-    yahoo_url = "http://ichart.finance.yahoo.com/table.csv"
-    yahoo_url+= "?s=%s&a=%s&b=%s&c=%s&d=%s&e=%s&f=%s"
-    yahoo_url = yahoo_url % ticker_tup
+    # try connecting to Yahoo! Finance and obtaining the data.
+    # On failure, print an error message.
 
-    print(yahoo_url)
+    try:
+        # change things like BRK.B to BRK-B
+        clean_ticker = ticker.replace("_","-")
+        clean_ticker = clean_ticker.replace(".","-")
+
+        # pandas dataframe of the historical data
+        yf_data = web.DataReader(clean_ticker,'yahoo',
+                                 start_date,end_date)
+        return yf_data
+    except Exception as e:
+        print("Could not download Yahoo data: %s" %e)
+
+def insert_daily_data_into_db(data_vendor_id, symbol_id, daily_data):
+    """
+    Takes a dataframe of daily data and adds it to the MySQL database.
+    Appends the vendor ID and symbol ID to the data.
+
+    daily_data: pandas dataframe of OHLC data (with adj_close and volume)
+    """
+
+    # Create the time now
+    now = datetime.datetime.now()
+
+    # Amend the data to inclide the vendor ID and symbol ID
