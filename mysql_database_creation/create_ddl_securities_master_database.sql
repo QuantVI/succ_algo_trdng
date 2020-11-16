c670773838d546541b653be7dce4eb2b8742119c
QuantVI <berkeleyvi@gmail.com>
2020-11-17 00:26:35 +0100

    including ddl to create the database, and 2nd user.

---
+++ mysql_database_creation/create_ddl_securities_master_database.sql
@@ -0,0 +1,12 @@
+create database securities_master;
+
+-- these next commands could be run in a mysql terminal
+-- we already have htis user set up
+
+/*
+mysql> USE securities_master;
+mysql> CREATE USER ’sec_user’@’localhost’ IDENTIFIED BY ’password’;
+mysql> GRANT ALL PRIVILEGES ON securities_master.* TO ’sec_user’@’localhost’;
+mysql> FLUSH PRIVILEGES;
+
+*/
